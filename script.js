58f377e261ed6f6caa2613a4c631dc6c4ff35eea
Joaoo <146839436+ooaoJ@users.noreply.github.com>
2024-03-19 15:48:48 -0300

    update game

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendDificulty(){
+    var selectElement = document.getElementById("game-options");
+    var dificulty = selectElement.value
+
+    switch (dificulty){
+        case "facil":
+            
+    }
+}
+
+
