72a37aa073a83e56bee1a03b7b531fd2748f0486
Joaoo <146839436+ooaoJ@users.noreply.github.com>
2024-03-20 21:47:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ function sendDificulty(){
 
     switch (dificulty){
         case "facil":
-            
+            jogoAdivinhacao(6, 15, true)
+        break;
+        case "medio":
+            jogoAdivinhacao(4, 30, true)
+        break;
+        case "hard":
+            jogoAdivinhacao(2, 50, false)
+        break;
+        default:
+            alert("Escolha uma dificuldade válida.")
+    }
+}
+
+function jogoAdivinhacao(chances, maxNumber, showTip){
+    var randomNum = Math.floor(Math.random() * (maxNumber + 1));
+    for (var i = 0; i < chances; i++){
+        var userAttempt = prompt('Tentativa '+(i+1)+'/'+chances+'. Digite um número entre 0 e ' + maxNumber + ": ")
     }
 }
 
